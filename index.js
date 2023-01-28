eda620b81d3ff611e694012afad93d35b5e547dd
Chang Wonseok <iampoem315@gmail.com>
2023-01-29 02:00:21 +0900

    Step 1 리팩토링 및 Step 2 구현

--- index.js
+++ index.js
@@ -1,50 +1,78 @@
-const ul = document.querySelector("ul");
+const ul = document.getElementById("list");
+const dimmedLayer = document.getElementById("dimmed-layer");
+const fragment = new DocumentFragment();
+let clickedItem = null;
 
+// list item 100개 생성
 for (let i = 1; i <= 100; i++) {
   const li = document.createElement("li");
   const div = document.createElement("div");
-  div.classList.add("test");
-  li.classList.add("li");
-  div.innerText = i;
+  const span = document.createElement("span");
+  span.innerText = i;
+  div.classList.add("content");
+  li.classList.add("item");
+  div.appendChild(span);
   li.appendChild(div);
-  ul.appendChild(li);
+  fragment.appendChild(li);
 }
+ul.appendChild(fragment);
 
-ul.addEventListener("mouseover", function (event) {
-  if (event.target.closest(".li")) {
-    const div = event.target.closest(".li").querySelector(".test");
-    if (event.target.closest(".li").previousElementSibling) {
-      event.target
-        .closest(".li")
-        .previousElementSibling.querySelector(".test").style.transform =
-        "translateX(20px)";
-    }
+// list item mouseover event handler
+const handleMouseover = (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
 
-    div.style.transform = "translate(40px)";
-    if (event.target.closest(".li").nextElementSibling) {
-      event.target
-        .closest(".li")
-        .nextElementSibling.querySelector(".test").style.transform =
-        "translateX(20px)";
-    }
-  }
-});
+  item.previousElementSibling
+    ?.querySelector(".content")
+    .classList.add("target-sibling");
+  item.querySelector(".content").classList.add("target");
+  item.nextElementSibling
+    ?.querySelector(".content")
+    .classList.add("target-sibling");
+};
 
-ul.addEventListener("mouseout", function (event) {
-  if (event.target.closest(".li")) {
-    const div = event.target.closest(".li").querySelector(".test");
-    if (event.target.closest(".li").previousElementSibling) {
-      event.target
-        .closest(".li")
-        .previousElementSibling.querySelector(".test").style.transform =
-        "translateX(0px)";
-    }
-    div.style.transform = "translate(0px)";
-    if (event.target.closest(".li").nextElementSibling) {
-      event.target
-        .closest(".li")
-        .nextElementSibling.querySelector(".test").style.transform =
-        "translateX(0px)";
-    }
-  }
-});
+// list item mouseout event handler
+const handleMouseout = (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
+  const content = item.querySelector(".content");
+  if (content.matches(".center")) return;
+
+  item.previousElementSibling
+    ?.querySelector(".content")
+    .classList.remove("target-sibling");
+  content.classList.remove("target");
+  item.nextElementSibling
+    ?.querySelector(".content")
+    .classList.remove("target-sibling");
+};
+
+// list item click event handler
+const handleClick = (event) => {
+  if (!event.target.matches(".content")) return;
+
+  event.target.classList.add("center");
+  clickedItem = event.target;
+  dimmedLayer.classList.remove("hide");
+};
+
+// dimmed layer click event handler
+const handleDimmedLayerClick = (event) => {
+  event.currentTarget.classList.add("hide");
+
+  const item = clickedItem.closest(".item");
+  item.previousElementSibling
+    ?.querySelector(".content")
+    .classList.remove("target-sibling");
+  clickedItem.classList.remove("target", "center");
+  item.nextElementSibling
+    ?.querySelector(".content")
+    .classList.remove("target-sibling");
+
+  clickedItem = null;
+};
+
+ul.addEventListener("mouseover", handleMouseover);
+ul.addEventListener("mouseout", handleMouseout);
+ul.addEventListener("click", handleClick);
+dimmedLayer.addEventListener("click", handleDimmedLayerClick);
